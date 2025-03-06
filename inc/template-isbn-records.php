b82e006682285e24b819698ec522d60d8c1f53f1
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-03-05 22:00:34 -0500

    Add files via upload

--- inc/template-isbn-records.php
+++ inc/template-isbn-records.php
@@ -35,6 +35,8 @@ if (is_user_logged_in()){
                     order by records.submitteddate desc
                     limit 3'; //change to 30 after testing
                     $results = $wpdb->get_results($wpdb->prepare($query, $isbn_numbers_table, $isbn_records_table, $posts_table, $book_products_table, $books_table, $order_items_table, $item_meta_table), ARRAY_A);
+                    echo 'test';
+                    echo var_dump($results);
                     if (count($results) > 0){
                         for ($i = 0; $i < count($results); $i++){
                             if ($i % 2 == 0){
